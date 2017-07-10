52b855948e3fbf1a46e62566e4aaf0a0a0203174
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-10 09:57:09 +0200

    add a test for zero length inputs

--- src/test/java/exercise8/utility/ArrayInterleaverTest.java
+++ src/test/java/exercise8/utility/ArrayInterleaverTest.java
@@ -10,6 +10,8 @@ public class ArrayInterleaverTest {
   public static final int[] INPUT_2 = new int[]{2, 6, 0, -12};
   public static final int[] INPUT_3 = new int[]{2, 10, 100};
 
+  public static final int[] INPUT_EMPTY = new int[]{};
+
   public static final int[] RESULT_OF_1_AND_2 = new int[]{1, 2, 7, 6, 9, 0, -5, -12};
   public static final int[] RESULT_OF_1_AND_3 = new int[]{1, 2, 7, 10, 9, 100};
 
@@ -21,5 +23,10 @@ public class ArrayInterleaverTest {
   @Test
   public void interleave_withDifferentLengthInputs() throws Exception {
     assertArrayEquals(RESULT_OF_1_AND_3, ArrayInterleaver.interleave(INPUT_1, INPUT_3));
+  }
+
+  @Test
+  public void interleave_withEmptyInputs() throws Exception {
+    assertArrayEquals(INPUT_EMPTY, ArrayInterleaver.interleave(INPUT_EMPTY, INPUT_EMPTY));
   }
 }
