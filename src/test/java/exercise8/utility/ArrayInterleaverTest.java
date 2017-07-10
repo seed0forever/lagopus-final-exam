82108a1b50c17a2fd2baecc807ab3e888062c3dc
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-10 09:54:23 +0200

    add a test for different input lengths

--- src/test/java/exercise8/utility/ArrayInterleaverTest.java
+++ src/test/java/exercise8/utility/ArrayInterleaverTest.java
@@ -8,11 +8,18 @@ public class ArrayInterleaverTest {
 
   public static final int[] INPUT_1 = new int[]{1, 7, 9, -5};
   public static final int[] INPUT_2 = new int[]{2, 6, 0, -12};
+  public static final int[] INPUT_3 = new int[]{2, 10, 100};
 
-  public static final int[] RESULT = new int[]{1, 2, 7, 6, 9, 0, -5, -12};
+  public static final int[] RESULT_OF_1_AND_2 = new int[]{1, 2, 7, 6, 9, 0, -5, -12};
+  public static final int[] RESULT_OF_1_AND_3 = new int[]{1, 2, 7, 10, 9, 100};
 
   @Test
-  public void interleave() throws Exception {
-    assertArrayEquals(RESULT, ArrayInterleaver.interleave(INPUT_1, INPUT_2));
+  public void interleave_withEqualLengthInputs() throws Exception {
+    assertArrayEquals(RESULT_OF_1_AND_2, ArrayInterleaver.interleave(INPUT_1, INPUT_2));
+  }
+
+  @Test
+  public void interleave_withDifferentLengthInputs() throws Exception {
+    assertArrayEquals(RESULT_OF_1_AND_3, ArrayInterleaver.interleave(INPUT_1, INPUT_3));
   }
 }
