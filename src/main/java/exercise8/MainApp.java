3dd1243684583f7b29378c82f6540df040c3fd5b
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-10 09:46:47 +0200

    add solution first version

---
+++ src/main/java/exercise8/MainApp.java
@@ -0,0 +1,16 @@
+package exercise8;
+
+import exercise8.utility.ArrayInterleaver;
+import java.util.Arrays;
+
+public class MainApp {
+
+  public static void main(String[] args) {
+    int[] input1 = new int[]{1, 7, 9};
+    int[] input2 = new int[]{2, 6, 0};
+
+    int[] result = ArrayInterleaver.interleave(new int[]{1, 7, 9}, new int[]{2, 6, 0});
+
+    System.out.println(Arrays.toString(result));
+  }
+}
---
+++ src/main/java/exercise8/utility/ArrayInterleaver.java
@@ -0,0 +1,23 @@
+package exercise8.utility;
+
+
+/*
+This function takes two arrays and mixes them like this:
+[1, 2, 3] and [6, 7, 8] becomes [1, 6, 2, 7, 3, 8]
+*/
+public class ArrayInterleaver {
+
+  public static int[] interleave(int[] numbers1, int[] numbers2) {
+    int shorterInputLength =
+        (numbers1.length < numbers2.length) ? numbers1.length : numbers2.length;
+    int resultLength = 2 * shorterInputLength;
+    int[] result = new int[resultLength];
+
+    for (int i = 0; i < shorterInputLength; i++) {
+      result[2 * i] = numbers1[i];
+      result[2 * i + 1] = numbers2[i];
+    }
+
+    return result;
+  }
+}
