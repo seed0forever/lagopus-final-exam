04c1f819bed99ba247971d3f234033e4dd3135df
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-10 10:14:48 +0200

    rename the function's parameters

--- src/main/java/exercise8/utility/SequenceUtil.java
+++ src/main/java/exercise8/utility/SequenceUtil.java
@@ -5,16 +5,16 @@ public class SequenceUtil {
 
   //  This function takes two arrays and mixes them like this:
   //  [1, 2, 3] and [6, 7, 8] becomes [1, 6, 2, 7, 3, 8]
-  public static int[] interleave(int[] numbers1, int[] numbers2) {
+  public static int[] interleave(int[] sequence1, int[] sequence2) {
 
     int shorterInputLength =
-        (numbers1.length < numbers2.length) ? numbers1.length : numbers2.length;
+        (sequence1.length < sequence2.length) ? sequence1.length : sequence2.length;
     int resultLength = 2 * shorterInputLength;
     int[] result = new int[resultLength];
 
     for (int i = 0; i < shorterInputLength; i++) {
-      result[2 * i] = numbers1[i];
-      result[2 * i + 1] = numbers2[i];
+      result[2 * i] = sequence1[i];
+      result[2 * i + 1] = sequence2[i];
     }
 
     return result;
